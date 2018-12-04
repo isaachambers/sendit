08e8acfc2e75a426ce5905225864e170d6cc683d
isaachambers <isaaceduardk@gmail.com>
2018-12-04 17:36:12 +0300

    adding angular material

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json())
 const port = 7000;
-
+app.get('/buttocks', (req,res)=>{
+    let buttocks = [ 'a','b','c']
+    res.send(buttocks)
+})
 app.listen(port, (err)=>{
     if(!err) console.log(`App running on port ${port}`);
-    
+
 })
--- sendit/src/app/app.module.ts
+++ sendit/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatButtonModule, MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
